916c1ed29b2e0f728f20e788c4afced03919ba51
RaphaelIniesta <raphael_iniesta@yahoo.com>
2024-02-18 22:39:31 -0300

    Add functions to save/delete items in SwiftData

--- MapDuck/MapDuck/ContentView.swift
+++ MapDuck/MapDuck/ContentView.swift
@@ -10,14 +10,52 @@ import SwiftData
 import CoreLocation
 
 struct ContentView: View {
+    
+    @Environment(\.modelContext) private var context
+    @Query private var items: [DataManager]
+    @State var text: String = ""
+    
+    //MARK: SwiftData functions
+    func addItem(title: String, memorie: String, lat: Double, long: Double, date: String) {
+        let item = DataManager(title: title, memorie: memorie, lat: lat, long: long, date: date)
+        
+        context.insert(item)
+    }
+    
+    func deleteItem(_ item: DataManager) {
+        context.delete(item)
+    }
+    
+    //TODO: Update items
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                HStack {
+                    TextField("Location", text: $text)
+                        .textFieldStyle(.roundedBorder)
+                    
+                    Button {
+                        addItem(title: text, memorie: text, lat: 0.0, long: 0.0, date: text)
+                    } label: {
+                        Text("Save")
+                    }
+                }
+                
+                List {
+                    ForEach(items, id: \.self) { item in
+                        Text(item.title)
+                    }
+                    .onDelete { indexes in
+                        for index in indexes {
+                            deleteItem(items[index])
+                        }
+                    }
+                }
+                
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
